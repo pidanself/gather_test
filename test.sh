a7eeabfc3045f7fd9884a01263123223ca0f801c
pidanself <pidanself@163.com>
2019-04-11 23:08:59 +0800

    154 pages

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 
 #there are totally 2398 pages in https://git.drupalcode.org/explore/projects
-thost="https://git.drupalcode.org/explore/projects?page="
+thost="https://git.drupalcode.org/explore/projects?archived=true&sort=name_asc&page="
 
 #if want to get all urls,replace '500' to '2398'
 
@@ -13,11 +13,10 @@ curl -o drupal.html  $rhost
 #all urls will be stored in ./drupal.com
 perl -ane 'while(m|<span class="project-name">([^<]*)</span>|g){print "https://git.drupalcode.org/project/$1\n"}' < drupal.html>> drupal.com
 
-if i%10==0
+if [ `expr $i % 10` -eq 0 ]
 then
     sleep 2
 fi
 
 done  
-
 
