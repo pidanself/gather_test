63d4fc95f179353d1ac633fbc0ba2b1d1c89ba18
pidanself <pidanself@163.com>
2019-04-11 22:02:57 +0800

    sleep 2 every 10 times

--- test.sh
+++ test.sh
@@ -4,15 +4,20 @@
 #there are totally 2398 pages in https://git.drupalcode.org/explore/projects
 thost="https://git.drupalcode.org/explore/projects?page="
 
-#if want to get all urls,change '10' to '2398'
+#if want to get all urls,replace '500' to '2398'
+
 for i in `seq 1 500`
 do   
 rhost="$thost$i"
 curl -o drupal.html  $rhost
+#all urls will be stored in ./drupal.com
 perl -ane 'while(m|<span class="project-name">([^<]*)</span>|g){print "https://git.drupalcode.org/project/$1\n"}' < drupal.html>> drupal.com
-sleep 2
+
+if i%10==0
+then
+    sleep 2
+fi
+
 done  
-
-
 
 
