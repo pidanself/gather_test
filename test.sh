f325361e4578bd12c8a0c81a04df1d6e1a6627dc
皮蛋 <42794155+pidanself@users.noreply.github.com>
2019-04-11 23:15:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,15 +4,15 @@
 #there are totally 2398 pages in https://git.drupalcode.org/explore/projects
 thost="https://git.drupalcode.org/explore/projects?archived=true&sort=name_asc&page="
 
-#if want to get all urls,replace '500' to '2398'
+#if want to get all urls,replace '154' to '2398'
 
-for i in `seq 1 500`
+for i in `seq 1 154`
 do   
 rhost="$thost$i"
 curl -o drupal.html  $rhost
 #all urls will be stored in ./drupal.com
 perl -ane 'while(m|<span class="project-name">([^<]*)</span>|g){print "https://git.drupalcode.org/project/$1\n"}' < drupal.html>> drupal.com
-
+#sleep 2 seconds every 10 times in case it gets blocked
 if [ `expr $i % 10` -eq 0 ]
 then
     sleep 2
